d5c3bf110ddd0dd08d682d044accf8dd308bcc59
shubham423 <shubh991172@gmail.com>
2023-03-19 18:27:14 +0530

    removed corrupted files

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,5 +1,6 @@
 const API_URL = 'http://localhost:8000';
 
+// Load planets and return as JSON.
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
   return await response.json();
