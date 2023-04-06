2a8a02578ad621f297287a0ccd89410762868406
shubham423 <shubh991172@gmail.com>
2023-04-06 20:36:04 +0530

    connect with mongo db

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,34 @@
 const http = require('http');
 
 const app = require('./app');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const { loadPlanetsData } = require('./models/planets.model');
 
-const PORT = process.env.PORT || 8000;
+const mongoUrl = process.env.MONGO_URL
 
+const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('MongoDB error!');
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
 
   server.listen(PORT, () => {
