76361b7b0209efa890da9d061ee809fbb10ff8c3
shubham423 <shubh991172@gmail.com>
2023-04-12 21:34:56 +0530

    saving launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,4 @@
-// const launches = require('./launches.mongo');
+const launchesDatabase = require('./launches.mongo');
 
 const launches = new Map();
 
@@ -15,7 +15,7 @@ const launch = {
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
   return launches.has(launchId);
@@ -31,6 +31,14 @@ function abortLaunchById(launchId) {
   aborted.upcoming = false;
   aborted.success = false;
   return aborted;
+}
+
+async function saveLaunch(launch) {
+  await launchesDatabase.updateOne({
+    flightNumber: launch.flightNumber,
+  }, launch, {
+    upsert: true,
+  });
 }
 
 function addNewLaunch(launch) {
