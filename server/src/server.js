df1e35337551d5fafe6020a5f5a52f31d423370c
shubham423 <shubh991172@gmail.com>
2023-04-12 22:58:52 +0530

    spacex api

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
 
@@ -29,6 +30,7 @@ async function startServer() {
     useUnifiedTopology: true,
   });
   await loadPlanetsData();
+  await loadLaunchData();
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
